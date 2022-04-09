685534919f00719d12faa99d4767a2c398318eba
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-09 00:03:05 -0500

    instalando swagger

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, NotFoundException, Inject } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { User } from '../entities/user.entity';
 import { Order } from '../entities/order.entity';
@@ -10,7 +11,7 @@ import { ProductsService } from './../../products/services/products.service';
 export class UsersService {
   constructor(
     private productsService: ProductsService,
-    @Inject('API_KEY') private apiKey: string,
+    private configService: ConfigService,
   ) {}
 
   private counterId = 1;
@@ -24,6 +25,7 @@ export class UsersService {
   ];
 
   findAll() {
+    // const apiKey = this.configService.get('API_KEY');
     return this.users;
   }
 
