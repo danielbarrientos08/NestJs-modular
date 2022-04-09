835a5dc0a5a1c460c163fdc4c25671dee225e045
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-08 22:03:18 -0500

    modulo global

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,26 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 @Module({
-  imports: [UsersModule, ProductsModule, DatabaseModule],
+  imports: [UsersModule, ProductsModule, DatabaseModule, HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise()
+          .then((resp) => {
+            return resp.data;
+          });
+        return tasks;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Inject } from '@nestjs/common';
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
