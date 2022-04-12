90315abfeec431ce79bb62dbc357d6df3aca0c16
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-12 01:53:22 -0500

    creando migraciones n->n

--- src/products/entities/category.entity.ts
+++ src/products/entities/category.entity.ts
@@ -1,5 +1,13 @@
-import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToMany,
+} from 'typeorm';
 
+import { Product } from './product.entity';
 @Entity()
 export class Category {
   @PrimaryGeneratedColumn()
@@ -7,4 +15,19 @@ export class Category {
 
   @Column({ type: 'varchar', length: 100, unique: true })
   name: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+
+  @ManyToMany(() => Product, (product) => product.categories)
+  products: Product[];
 }
